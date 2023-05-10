f5f1c9f6a11a3315c7a36fbba495c54fb7954681
Jon Cochran <46171023+PythonButcher@users.noreply.github.com>
2023-05-09 22:05:01 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,157 +1,160 @@
-# Import libraries
+import os
+import json
 import dash
-from dash import Dash
 from dash import dcc
 from dash import html
 import dash_bootstrap_components as dbc
-from dash import Dash, dash_table
+from dash import dash_table
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 import pandas as pd
 import plotly.express as px
-# --------------------------------------------------------------------------------------------
-# Left off here, trying to get map(iframe) to be beside datatable !!!!!!!!!!!!!
+import plotly.io as pio
+import folium
+from folium import Map, Marker, Popup, IFrame
+from folium import Icon
+import branca
 
+# Set default plotly theme
+pio.templates.default = 'plotly_dark'
+
+# Read data
 df = pd.read_csv('data/VTmoviesCoords.csv')
 year_min = df['Year'].min()
 year_max = df['Year'].max()
 
+# Read GeoJSON file
+with open('vermont-county-boundaries.geojson', 'r') as f:
+    vt_geojson = json.load(f)
 
 app = dash.Dash(
     __name__,
-    external_stylesheets=[dbc.themes.MINTY],
+    external_stylesheets=[dbc.themes.SLATE],
     meta_tags=[
         {"name": "viewport", "content": "width=device-width, initial-scale=1"}])
 server = app.server
 
+def create_folium_map(filtered_df):
+    vt_map = Map(location=[filtered_df['Latitude'].mean(), filtered_df['Longitude'].mean()], zoom_start=7, control_scale=True)
 
-# Layout: With Bootstrap
-# --------------------------------------------------------------------------------------------
-# Start the layout of app
-# Put title and format it in column
+    # Add state and county borders
+    folium.GeoJson(
+        vt_geojson,
+        style_function=lambda x: {'fillColor': 'transparent', 'color': 'blue' if 'cntyname' not in x['properties'] else 'green', 'weight': 2 if 'cntyname' not in x['properties'] else 1.5}
+    ).add_to(vt_map)
+
+    for index, row in filtered_df.iterrows():
+        popup_html = f"""
+        <div style="font-family: Arial, sans-serif; font-size: 16px;">
+            <h4><b>{row['Title']}</b></h4>
+            <p>Year: {row['Year']}<br>
+            Town: {row['Town']}</p>
+            <a href="{row['URL']}" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/64px-IMDB_Logo_2016.svg.png" width="70" height="35" alt="IMDb"></a>
+            <p style="font-size: 14px; color: #666;"><i>Click the link to visit the movie's IMDb page.</i></p>
+        </div>
+        """
+        iframe = IFrame(html=popup_html, width=300, height=180)
+        popup = Popup(iframe, max_width=300)
+        Marker(location=[row['Latitude'], row['Longitude']], popup=popup).add_to(vt_map)
+
+    return vt_map
+
 app.layout = dbc.Container([
     dbc.Row([
         dbc.Col(html.H1('Movies Filmed in Vermont - Dashboard',
                         className="text-center text-primary, mb-4"),
                 width=12)
-
     ]),
-    # Title above the map
     dbc.Row([
-        dbc.Col(html.H5('Interactive Map of Filming Locations'))
-
-
-
-
+        dbc.Col(html.H5('Interactive Map of Filming Locations', className="text-center"))
     ]),
-    # html Iframe loading in the map file
     dbc.Row([
-        dbc.Col(html.Div([html.Iframe(id='map', srcDoc=open(
-            'data/Updated_Movies_In_Vermont.html', 'r').read(), width='99%', height='620')], style={'width': '95%', 'display': 'inline-block'
-                                                                                            }), style={'width': '49%', 'display': 'inline-block'}),
-        # New column where the graph will be place - beside the map
-        dbc.Col(
-            # Create a graph and choose desired parameters
-            dcc.Graph(
-                id='graph',
-                figure={
-                    'data': [
-                        {'x': df['Title'],
-                         'y': df['Box Office Gross'], 'marker': {"color": '#005710'}, 'type': 'bar'},
-                    ],
-                    'layout': {
-                        'title': 'Movie Titles | Box Office numbers',
-                        'height': 700,
-                        'width': 910,
+        dbc.Col(html.Div([
+            dcc.Tabs(id='tabs', children=[
+                dcc.Tab(label='Map', children=[
+                    html.Iframe(id='map', width='100%', height='600')
+                ]),
+                dcc.Tab(label='DataTable and Chart', children=[
+                    dbc.Row([
+                        dbc.Col(
+                            dcc.Graph(
+                                id='graph',
+                                figure={
+                                                                        'data': [
+                                        {'x': df['Title'],
+                                         'y': df['Box Office Gross'], 'marker': {"color": '#005710'}, 'type': 'bar'},
+                                    ],
+                                }),
+                            width=12, lg=6),
+                        dbc.Col(
+                            html.Div(
+                                dash_table.DataTable(id='table-container',
+                                                     columns=[{'name': col, 'id': col}
+                                                              for col in df.columns],
+                                                     data=df.to_dict('records'),
+                                                     style_cell={
+                                                         'font_family': 'Arial',
+                                                         'padding': '1.rem',
+                                                         'backgroundColor': '#f4f4f2',
+                                                         'headers': True
+                                                     },
+                                                     style_header={
+                                                         'backgroundColor': 'white',
+                                                         'fontWeight': 'bold',
+                                                         'backgroundColor': 'rgb(30, 30, 30)',
+                                                         'color': 'white',
+                                                         'fontWeight': 'bold'
 
-                    },
-
-                }, style={'width': '49%', 'display': 'inline-block'}),
-
-
-        )
+                                                     },
+                                                     style_data={
+                                                         'backgroundColor': 'rgb(50, 50, 50)',
+                                                         'color': 'white'
+                                                     },
+                                                     style_table={
+                                                         'maxHeight': '400px',
+                                                         'maxWidth': '1900px',
+                                                         'overflowX': 'scroll',
+                                                         'overflowY': 'scroll'
+                                                     })),
+                            width=12, lg=6)
+                    ]),
+                ]),
+            ]),
+        ]), width={'size': 10, 'offset': 1})
     ]),
-    # Small header for the range slider
     dbc.Row([
-        dbc.Col(html.H6('Range-Slider: Use this to filter the data below.'))
-
+        dbc.Col(html.H6('Range-Slider: Filter the years.', className="text-center"))
     ]),
-    # Range-slider
-    # Provides a method to be able to filter years based on a range
     dbc.Row([
         dbc.Col(dcc.RangeSlider(id='year-slider',
                                 min=year_min,
                                 max=year_max,
                                 value=[year_min, year_max],
                                 marks={i: str(i) for i in range(year_min, year_max+1)}),
-
                 )
-
-
     ]),
-
-
-
-    dbc.Row([
-            dbc.Col(html.Div(
-                # Datatable of the actual dataset
-                # Use range slider to view films in particular desired filming Years
-                dash_table.DataTable(id='table-container',
-                                     columns=[{'name': col, 'id': col}
-                                              for col in df.columns],
-                                     data=df.to_dict('records'),
-                                     # CSS code for formatting
-                                     style_cell={
-                                         'font_family': 'Arial',
-                                         'padding': '1.rem',
-                                         'backgroundColor': '#f4f4f2',
-                                         'headers': True
-                                     },
-                                     style_header={
-                                         'backgroundColor': 'white',
-                                         'fontWeight': 'bold',
-                                         'backgroundColor': 'rgb(30, 30, 30)',
-                                         'color': 'white',
-                                         'fontWeight': 'bold'
-
-                                     },
-                                     style_data={
-                                         'backgroundColor': 'rgb(50, 50, 50)',
-                                         'color': 'white'
-                                     },
-                                     style_table={
-                                         'maxHeight': '400px',
-                                         'maxWidth': '1900px',
-                                         'overflowX': 'scroll',
-                                         'overflowY': 'scroll'
-                                     })))
-            ])
-
 ], fluid=True)
 
-
-# Call-back components
-# --------------------------------------------------------------------------------------------
-
-@ app.callback(
-    Output('table-container', 'children'),
+@app.callback(
+    Output('graph', 'figure'),
     Input('year-slider', 'value')
 )
-def update_table(value):
-
-    return str(value)
-
-# Call back function to connect the datatable and range slider
-@ app.callback(
-    Output('table-container', 'data'),
-    Input('year-slider', 'value')
-)
-# Function for updating the datatable
-def update_datable(selected_years):
+def update_bar_chart(selected_years):
     filtered_df = df[(df['Year'] >= selected_years[0]) &
                      (df['Year'] <= selected_years[1])]
-    return filtered_df.to_dict('records')
+    figure = px.bar(filtered_df, x='Title', y='Box Office Gross')
+    return figure
 
-# Need this to run the application
+@app.callback(
+    Output('map', 'srcDoc'),
+    Input('year-slider', 'value')
+)
+def update_map(selected_years):
+    filtered_df = df[(df['Year'] >= selected_years[0]) &
+                     (df['Year'] <= selected_years[1])]
+    vt_map = create_folium_map(filtered_df)
+    return vt_map.get_root().render()
+
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
+
